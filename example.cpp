56d5cf20baacd26f32f2b69ac39d771ab51b02b4
Gaurav <gst1545@gmail.com>
2025-07-05 14:06:23 +0530

    add

--- example.cpp
+++ example.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main() {
     cout << "Hello, this is lcal repo!" << endl;
-    cout<<"Feature1 change"<<endl;
+    cout<<"Feature1 change {new feature}"<<endl;
     return 0;
 }
